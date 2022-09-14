feeb34551311319df4180a4a6413cc7b5bbc9a77
ThoriqOik <95134513+ThoriqOIk@users.noreply.github.com>
2022-09-15 00:36:56 +0700

    Add files via upload

---
+++ Muhammad Thoriq Nyoba/src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Size of chair :"<<sizeof(char)<<endl;
+    cout<<"Size of int :"<<sizeof(int)<<endl;
+    cout<<"Size of short int :"<<sizeof(short int)<<endl;
+    cout<<"Size of long int :"<<sizeof(long int)<<endl;
+    cout<<"Size of float :"<<sizeof(double)<<endl;
+    cout<<"Size of wchar_t :"<<sizeof(wchar_t)<<endl;
+}
